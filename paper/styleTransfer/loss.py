9071b898e9c51442bb6cc58f3662ccf594634f7d
mia2583 <mia2583@pusan.ac.kr>
2025-01-16 20:06:14 +0900

    feat: StyleTransfer - model, loss

---
+++ paper/styleTransfer/loss.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# content loss - vgg19 feature map
+class ContentLoss(nn.Module):
+    def __init__(self):
+        super(ContentLoss, self).__init__()
+    
+    def forward(self, x:torch.Tensor, y:torch.Tensor):
+        loss = F.mse_loss(x, y)
+        return loss
+
+
+# style loss - gram matrix
+class StyleLoss(nn.Module):
+    def __init__(self):
+        super(StyleLoss, self).__init__()
+
+    def gram_matrix(self, x:torch.Tensor):
+        b, c, h, w = x.size()
+        features = x.view(b, c, h*w) # (b, N,M)
+        features_T = features.traspose(1, 2) # (b, M,N)
+        G = torch.matmul(features, features_T) # (b,N, N)
+        return G.div(b*c*h*w)
+    
+    def forward(self, x, y):
+        Gx = self.gram_matrix(x)
+        Gy = self.gram_matrix(y)
+        loss = F.mse_loss(Gx, Gy)
+        return loss
---
+++ paper/styleTransfer/models.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from torchvision.models import vgg19
+
+# 사용할 conv 레이어
+conv = {
+    'conv1_1' : 0,  # style
+    'conv2_1' : 5,  # style
+    'conv3_1' : 10,  # style
+    'conv4_1' : 19,  # style
+    'conv5_1' : 28,  # style
+    'conv4_2' : 21,  # content
+}
+
+
+class StyleTransfer(nn.Module):
+    def __init__(self):
+        super(StyleTransfer, self).__init__()
+        #VGG 모델을 load해서 사용할 conv 레이어 분리
+        self.vgg19_model = vgg19(pretrained=True) # pretrain 된 VGG19 모델 사용
+        self.vgg19_features = self.vgg19_model.features
+
+        self.style_layer = [conv['conv1_1'], conv['conv2_1'], conv['conv3_1'], conv['conv4_1'], conv['conv5_1']]
+        self.content_layer = [conv['conv4_2']]
+
+    def forward(self, x, layer_type:str):
+        features = []
+        if layer_type == 'style':
+            layer = self.style_layer
+        elif layer_type == 'content':
+            layer = self.content_layer
+        else : 
+            print("No layer type : Choose 'style' or 'content'")
+            return 
+        # 종료 ㄱ관련 정의하기
+
+        for i in range(len(self.vgg19_features)):
+            x = self.vgg19_features[i](x)
+            if i in layer :
+                features.append(x)
+
+        return features
